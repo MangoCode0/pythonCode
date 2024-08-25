7c3c5d70ef56591aa8ea49a2d08cb254cafa7a1b
MangoCode0 <malik.asgar@outlook.com>
2024-08-25 19:42:07 +0530

    adding Odd Even file

---
+++ OddEven.py
@@ -0,0 +1,12 @@
+
+# age=int(input("Enter your age:"))
+# if (age >= 18):
+#     print("You are eligibility to give votes")
+# else:
+#     print("You are not eligibility to give votes")
+
+n=int(input("Enter a no.:"))
+if (n%2==0):
+    print(n,"is Even no.")
+else:
+    print(n,"is Odd no.")
